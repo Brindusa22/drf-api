bbef31cdc2ae1e04b85f2f0f5994bcb8a8086dc2
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-06-04 11:54:11 +0000

    Add another profile filter

--- profiles/views.py
+++ profiles/views.py
@@ -74,7 +74,8 @@ class ProfileList(generics.ListAPIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__following__followed__profile',
+        'owner__following__followed__profile', # filter profiles that are following a profile gived its id
+        'owner__followed__owner__profile', # get all profiles that are followed by a profile, given its id
     ]
     ordering_fields = [
         'posts_count',
