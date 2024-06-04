e070521a20ac4f36695ec5ab6e28573a1614e20b
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-06-04 11:34:46 +0000

    Add post and profile filters using DjangoFilterBackend

--- profiles/views.py
+++ profiles/views.py
@@ -52,6 +52,7 @@
 
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -69,7 +70,11 @@ class ProfileList(generics.ListAPIView):
         following_count = Count('owner__following', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
     ]
     ordering_fields = [
         'posts_count',
